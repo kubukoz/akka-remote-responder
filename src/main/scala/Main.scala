3369ab032c886cd44c6a4f09cdcdef0e3bbef897
Jakub Kozłowski <kubukoz@gmail.com>
2015-12-23 14:20:08 +0100

    Sending remote message sample

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,29 @@
 import akka.actor._
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
 
 object Main {
   def main(args: Array[String]) {
-    ActorSystem("akka-remote-responder").actorOf(Props[Responder], "responder")
+    implicit val system = ActorSystem("akka-remote-responder")
+    implicit val dispatcher = system.dispatcher
+
+    system.actorOf(Props[Responder], "responder")
+    val selection = system.actorSelection("akka.tcp://akka-remote-responder@localhost:2552/user/responder")
+
+    implicit val timeout = Timeout(5.seconds)
+
+    (selection ? Identify(0)).mapTo[ActorIdentity].map {
+      case ActorIdentity(_, Some(ref)) =>
+        println("Found remote actor!")
+        (ref ? "Hello").mapTo[String] foreach println
+
+      case ActorIdentity(_, None) => println("Didn't find remote actor")
+    }
   }
 }
 
-class Responder extends Actor{
+class Responder extends Actor {
   override def receive: Receive = {
     case s: String => sender ! s"$s :)"
   }
